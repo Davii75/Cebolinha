33fb80a8c90d2166750c2648d5eb0e6f6843b2d2
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-17 10:31:16 -0300

    Finaliza o programa Cebolinha

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase;
+string repeticao;
+
+Console.WriteLine("Digite uma frase para o Cebolinha repetir:");
+frase = Console.ReadLine();
+Console.WriteLine("Cebolinha:");
+repeticao = frase
+            .Replace("r", "l")
+            .Replace("R", "L");
+Console.WriteLine($"{repeticao}");
